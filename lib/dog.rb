2e4c3bfdf372303fb5d75cf7903f49a0e0ab943f
Evan Smith <professionalemail@zoho.com>
2020-04-09 05:58:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,14 @@
 class Dog
-  @breed = "Mutt"
   def initialize(name,breed)
     @name = name
+    @breed = breed
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
     @breed = breed
   end
 end
